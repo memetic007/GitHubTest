e8c7d260c8d22e5ad153acd3fd551818d4ba22f7
Jim Rutt <jimrutt@gmail.com>
2016-06-26 14:26:45 -0400

    First Commit

---
+++ GitHubTest/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Static Dim x As Integer = 0
+        x = x + 1
+
+        Label1.Text = "Button Pressed " + x.ToString
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+
+    End Sub
+End Class
