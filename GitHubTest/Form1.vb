a92942425d2e6b22216b838e75149cd1e5cbaffc
Jim Rutt <jimrutt@gmail.com>
2016-06-26 14:53:10 -0400

    x = 14

--- GitHubTest/Form1.vb
+++ GitHubTest/Form1.vb
@@ -8,7 +8,7 @@
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim x As Integer
-        x = 4
+        x = 12
         Application.Exit()
 
     End Sub
