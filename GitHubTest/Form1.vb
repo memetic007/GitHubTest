dfb38b9248131c00a8ec20862716a8b4ff138b9e
Jim Rutt <jimrutt@gmail.com>
2016-06-27 09:25:33 -0400

    x = 111111     y = 888888

--- GitHubTest/Form1.vb
+++ GitHubTest/Form1.vb
@@ -8,8 +8,8 @@
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim y, x As Integer
-        x = 555555
-        y = 999999
+        x = 111111
+        y = 888888
         Application.Exit()
 
     End Sub
